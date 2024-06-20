52b3d5b9f2c15136d2481e8260a206d228dd52ed
pro100glebb <101585176+pro100glebb@users.noreply.github.com>
2024-06-20 05:21:50 -0700

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -2,6 +2,6 @@
 def upp_string(str):
     print(str.upper())
 
-"функция для смены регистра заглавных букв!!!"
+"функция для смены регистра заглавных букв!!!!!!!!!!"
 def upp_first_letter (str):
     print(str.title())
